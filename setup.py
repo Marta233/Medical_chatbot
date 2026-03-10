9e70d45b528a52f98b0ddfd206249f3bec2854eb
Marta <martaassefa23@gmail.com>
2026-03-10 11:56:57 +0300

    requirm set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_chatbot",
+    version="0.1",
+    author="Marta A.",
+    author_email="martaassefa23@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
