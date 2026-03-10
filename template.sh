93b7e5bca316e50e5b2e982fd6fca1a2af4b8dae
Marta <martaassefa23@gmail.com>
2026-03-10 11:05:25 +0300

    folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Directory
+mkdir -p src
+mkdir -p reasearch
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch App.py
+touch reasearch/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo 'file and directory created successfully'
